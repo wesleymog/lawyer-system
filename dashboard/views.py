eef51e4cd28b474717e109aaddcc036361c7e065
wesley <wesleymotabr@gmail.com>
2020-03-25 20:40:53 -0300

    +Lista de usuários com edição e criação

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from users.models import Users
 
 def helloWorld(request):
     return HttpResponse('Hello World')
 
 def home(request):
-    return render(request, 'dashboard/home.html')
+    users = Users.objects.all()
+    context = {'users':users}
+
+    return render(request, 'dashboard/home.html', context)
